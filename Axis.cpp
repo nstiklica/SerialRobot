c6f9baa455ac935eb6ebcc133e8975cc49f0a5e6
nstiklica <nstiklica@live.com>
2023-01-24 22:22:09 +0100

    Added Axis class

---
+++ Axis.cpp
@@ -0,0 +1,40 @@
+#include "Axis.h"
+
+int Axis::Init(int pinNumber, int stepDelay, int startPosition, int minAngle, int maxAngle)
+{
+    m_servo.attach(pinNumber);
+    m_stepDelay = stepDelay;
+    m_startPosition = startPosition;
+    m_minAngle = minAngle;
+    m_maxAngle = maxAngle;
+}
+
+void Axis::SetAngle(int angle)
+{
+    m_finalAngle = angle;
+}
+
+void Axis::SetStep(int step)
+{
+    m_step = step;
+}
+
+void Axis::Move()
+{
+    int currentPosition = m_servo.GetPosition();
+
+    if (currentPosition == m_finalAngle)
+    {
+        return;
+    }
+    else if (currentPosition < m_finalAngle)
+    {
+        currentPosition++;
+        m_servo.SetPosition(currentPosition);
+    }
+    else if (currentPosition > m_finalAngle)
+    {
+        currentPosition--;
+        m_servo.SetPosition(currentPosition);
+    }
+}
---
+++ Axis.h
@@ -0,0 +1,29 @@
+#ifndef __AXIS_H__
+#define __AXIS_H__
+
+#include "MyServo.h"
+
+class Axis
+{
+private:
+    MyServo m_servo;
+
+    int m_id;
+    int m_finalAngle;
+    int m_stepDelay;
+    int m_minAngle;
+    int m_maxAngle;
+    int m_startPosition;
+    int m_currentPosition;
+    int m_step = 1;
+
+public:
+    Axis() {};
+
+    int Init(int pinNumber, int stepDelay, int startPosition, int minAngle, int maxAngle);
+    void SetAngle(int angle);
+    void SetStep(int step);
+    void Move();
+}
+
+#endif
